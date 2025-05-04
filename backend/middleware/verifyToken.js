fb932c7e1dcb8045a87ed530a24676343acb86a0
WeiHungLoh <weihung789@hotmail.com>
2025-05-04 23:55:18 +0800

    Add code comment for verifyToken component

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -2,7 +2,10 @@ const jwt = require("jsonwebtoken");
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
 const verifyToken = (req, res, next) => {
-  // Split the current "Bearer <token>" under headers authorisation section to get token
+  // Extract JWT from the Authorization header in the format: "Bearer <token>".
+  // The token is sent by the frontend (after sign in) and included in requests 
+  // to protected routes like /exam or /assignment.
+  // It is initially saved in localStorage upon successful sign-in.
   const token = req.headers.authorization?.split(" ")[1]; 
 
   if (!token) {
@@ -11,7 +14,8 @@ const verifyToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
-    // Store existing id, email, new iat and expiry
+
+    // Save decoded token data (userid and email) so it can be used in protected routes (/exam and /assignment)
     req.user = decoded; 
     next();
   } catch (err) {
