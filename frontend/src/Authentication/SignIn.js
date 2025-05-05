faf1e3a81e010ccb9a228a103a59dd4eb1490b07
WeiHungLoh <weihung789@hotmail.com>
2025-05-06 00:39:24 +0800

    Move SignIn and SignUp components to Authentication folder

---
+++ frontend/src/Authentication/SignIn.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const SignIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSignIn = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch("/auth/signin",
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ email, password })
+                }
+            );
+            
+            const data = await res.json();
+            if (!res.ok) {
+                alert("Failed to sign in: " + data.message);
+                return;
+            }
+
+            // Save access token so we can use it for routes protected by access token
+            localStorage.setItem("token", data.token);
+            navigate("/addassignment");
+        } catch (error) {
+            alert("Failed to sign in! " + error.message);
+        }
+    }
+
+    const toggleSignUp = async (e) => {
+        navigate("/signup");
+    }
+
+    return (
+        <div className="signin">
+            <h2>Sign in to Academic Tracker</h2>
+            <form onSubmit={handleSignIn}>
+
+                <label>Email</label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                />
+
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                />
+
+                <button type="submit">Login</button>
+                <p onClick={toggleSignUp}>
+                    Don’t have an account? Create one
+                </p>
+
+            </form>
+        </div>
+    );
+};
+
+export default SignIn;
---
+++ frontend/src/Authentication/SignUp.js
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Notification from '../Notification';
+
+const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [notification, setNotification] = useState(null);
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch("/auth/signup", 
+                {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password })
+                }
+            );
+
+            const message = await res.text();
+
+            if (!res.ok) {
+                alert("Failed to sign up: " + message);
+                return;
+            }
+
+            setNotification("Sign up succesful! Redirecting you to login page")
+            setTimeout(() => {
+                setNotification(null);
+                navigate("/");
+            }, 1500);
+        } catch (error) {
+            alert("Failed to signed up. " + error.message);
+        }
+    }
+
+    const toggleSignIn = async (e) => {
+        navigate("/");
+    }
+
+    return (
+        <div className="signup">
+            <h2>Sign up for Academic Tracker</h2>
+            <form onSubmit={handleSignUp}>
+
+                <label>Email</label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                />
+
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                />
+
+                <button type="submit">Sign up</button>
+
+                <p onClick={toggleSignIn}>
+                    Already have an account? Login here
+                </p>
+            </form>
+            <Notification message={notification}/>
+        </div>
+    );
+
+};
+
+export default SignUp;
