b1b4d5d3bfb5c65a16648acd85ed782953d06c89
WeiHungLoh <weihung789@hotmail.com>
2025-05-25 16:15:54 +0800

    Change db connection name

--- backend/db.js
+++ backend/db.js
@@ -5,11 +5,13 @@ dotenv.config();
 const connectDB = async() => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "academic_tracker",
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
 
     console.log(`Mongo DB connected: ${conn.connection.host}`);
+    console.log(`Database connected: ${conn.connection.db.databaseName}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit();
