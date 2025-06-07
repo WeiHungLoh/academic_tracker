125b1b617021d7a51578bd29b15bc0dc69a8714c
WeiHungLoh <weihung789@hotmail.com>
2025-06-07 14:44:48 +0800

    Edit eslint config for frontend and backend

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,30 +1,30 @@
-// Taken from https://eslint.org/docs/latest/use/configure/configuration-files
-import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
+// Taken from: https://eslint.org/docs/latest/use/configure/configuration-files
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
 	{
-		files: ["**/*.js"],
+		files: ['**/*.js'],
 		plugins: {
-			js,
+			js
 		},
-		extends: ["js/recommended"],
     languageOptions: {
       // ignores errors pertaining to 'console' or 'process' not defined
       globals: {
-        console: "readonly",
-        process: "readonly",
+        console: 'readonly',
+        process: 'readonly',
       },
     },
 		rules: {
+      ...js.configs.recommended.rules,
       // [rule severity, style to enforce]
-			"no-unused-vars": "warn",
-      "quotes": ["error", "single"],
-      "semi": ["error", "never"],
-      "no-trailing-spaces": "error",
-      "space-before-function-paren": ["error", "always"],
-      "eol-last": ["error", "always"],
-      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }]
+			'no-unused-vars': 'warn',
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'no-trailing-spaces': 'error',
+      'space-before-function-paren': ['error', 'always'],
+      'eol-last': ['error', 'always'],
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }]
 		},
 	},
-]);
+])
